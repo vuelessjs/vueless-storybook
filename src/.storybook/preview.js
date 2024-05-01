a058c02ed4b6f6645ee5606a4c1e8a478fd8258d
Ivan Hridniev <i.ivan.gridnev@gmail.com>
2024-05-01 22:33:30 +0800

    updated storybook template after vueless updates.

--- src/.storybook/preview.js
+++ src/.storybook/preview.js
@@ -1,33 +1,20 @@
 import { setup } from "@storybook/vue3";
+
 import { backgrounds, docs, layout } from "./configs/main.config";
 import { vue3SourceDecorator } from "./decorators/vue3SourceDecorator";
 
 // Vue plugins
-import { createStore } from "vuex";
 import { createVueless } from "vueless";
-import NotifyServiceDefault from "vueless/ui.notify/services";
 
 // Tailwind styles
 import "./index.pcss";
-
-// Common stores
-import loader from "vueless/ui.loader-rendering/store";
-import loaderTop from "vueless/ui.other-loader-top/store";
-import breakpoint from "vueless/ui.viewport/store";
-
-// Create store instance
-const store = createStore({
-  modules: { loader, loaderTop, breakpoint },
-});
 
 // Create vueless instance
 const vueless = createVueless();
 
 // Create storybook app instance
 const storybookApp = (app) => {
-  app.use(store);
   app.use(vueless);
-  app.use(new NotifyServiceDefault().notifyInstance);
 };
 
 // Setup storybook
@@ -42,9 +29,9 @@ export default {
     backgrounds,
     options: {
       storySort: (a, b) =>
-          a.id === b.id
-              ? 0
-              : a.name === "Docs" && a.id.localeCompare(b.id, undefined, { numeric: true }),
+        a.id === b.id
+          ? 0
+          : a.name === "Docs" && a.id.localeCompare(b.id, undefined, { numeric: true }),
     },
   },
 };
