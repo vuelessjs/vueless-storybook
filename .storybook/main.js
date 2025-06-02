203887216c1af36dc44f77b85d880c93e625ff2f
Johnny Grid <ilevel.ws@gmail.com>
2025-06-02 09:58:32 +0800

    added folder to the root for sb update

---
+++ .storybook/main.js
@@ -0,0 +1,29 @@
+/** @type { import('@storybook/vue3-vite').StorybookConfig } */
+export default {
+  stories: [
+    "../node_modules/vueless/**/stories.{js,jsx,ts,tsx}",
+    "../node_modules/vueless/**/docs.mdx",
+    /* Define a path to your own component stories. */
+    // "../src/**/stories.{js,jsx,ts,tsx}",
+    // "../src/**/docs.mdx",
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "storybook-dark-mode",
+    "@storybook/addon-themes",
+  ],
+  framework: {
+    name: "@storybook/vue3-vite",
+    options: {
+      builder: {
+        viteConfigPath: ".storybook/vite.config.js",
+      },
+    },
+  },
+  env: (config) => ({
+    ...config,
+    BASE_URL: "/",
+  }),
+};
