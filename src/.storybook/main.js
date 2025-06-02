79687f49c9be4932d591fb2b5b6ea18e345a8c30
Johnny Grid <ilevel.ws@gmail.com>
2025-06-02 10:12:01 +0800

    updated storybook to v9

--- src/.storybook/main.js
+++ src/.storybook/main.js
@@ -9,10 +9,9 @@ export default {
   ],
   addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
     "storybook-dark-mode",
     "@storybook/addon-themes",
+    "@storybook/addon-docs"
   ],
   framework: {
     name: "@storybook/vue3-vite",
