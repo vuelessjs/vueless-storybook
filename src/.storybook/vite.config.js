84edb3ad218d42e2768e9f3a9e6e3ad59ce9f4b8
Johnny Grid <ilevel.ws@gmail.com>
2025-07-29 15:04:39 +0800

    Added `STORYBOOK_ENV` to Vite config for Vueless plugin.

--- src/.storybook/vite.config.js
+++ src/.storybook/vite.config.js
@@ -3,9 +3,10 @@ import { defineConfig } from "vite";
 // Plugins
 import Vue from "@vitejs/plugin-vue";
 import { Vueless, TailwindCSS } from "vueless/plugin-vite";
+import { STORYBOOK_ENV } from "vueless/constants.js";
 
 export default defineConfig({
-  plugins: [Vue(), TailwindCSS(), Vueless()],
+  plugins: [Vue(), TailwindCSS(), Vueless({ env: STORYBOOK_ENV })],
   optimizeDeps: {
     include: [
       "cva",
