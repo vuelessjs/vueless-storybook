7e652d0807f5b28f2db4759d8f0cc8564f8ddfc8
Johnny Grid <ilevel.ws@gmail.com>
2025-08-26 23:57:17 +0800

    Refactored Storybook initialization: modularized commands, added constants, and cleaned up `index.js` (fixed issue for Windows).

--- src/bin/index.js
+++ src/bin/index.js
@@ -1,89 +1,25 @@
 #!/usr/bin/env node
+
 /* eslint-disable no-console */
 
-import path from "node:path";
-import { cwd } from "node:process";
 import { styleText } from "node:util";
-import fs, { promises } from "node:fs";
-import { detectTypeScript } from "vueless/utils/node/helper.js";
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
-const sourceTs = path.join(__dirname, "..", ".storybook-ts");
-const sourceJs = path.join(__dirname, "..", ".storybook-js");
-const target = path.join(cwd(), ".storybook");
+import { commands } from "./commands/index.js";
+import { DEFAULT_EXIT_CODE, FAILURE_CODE } from "./constants.js";
 
-const hasTypeScript = await detectTypeScript();
+const [command, ...options] = process.argv.slice(2);
 
-hasTypeScript ? copyStorybookPreset(sourceTs, target) : copyStorybookPreset(sourceJs, target);
-
-await addStorybookCommands();
-
-/**
- * Copy Storybook preset to target directory.
- * @param {string} source - Source directory.
- * @param {string} target - Target directory.
- * @param {Object} options - Options object.
- * @param {boolean} options.consoles - Show console messages.
- */
-function copyStorybookPreset(source, target, { consoles = true } = {}) {
-  if (fs.existsSync(target)) {
-    const timestamp = new Date().valueOf();
-    const renamedTarget = `${target}-backup-${timestamp}`;
-
-    fs.renameSync(target, renamedTarget);
-
-    const warnMessage = styleText(
-      "yellow",
-      `Current Storybook preset backed into: '${path.basename(renamedTarget)}' folder. Remove it before commit.`,
-    );
-
-    console.log(warnMessage);
+try {
+  if (!command || command === "undefiend") {
+    process.exit(DEFAULT_EXIT_CODE);
   }
 
-  fs.mkdirSync(target, { recursive: true });
-
-  const files = fs.readdirSync(source);
-
-  files.forEach((file) => {
-    const srcFile = path.join(source, file);
-    const destFile = path.join(target, file);
-    const stat = fs.lstatSync(srcFile);
-
-    stat.isDirectory()
-      ? copyStorybookPreset(srcFile, destFile, { consoles: false })
-      : fs.copyFileSync(srcFile, destFile);
-  });
-
-  if (consoles) {
-    const successMessage = styleText(
-      "green",
-      `Storybook preset successfully saved into '${path.basename(target)}' folder.`,
-    );
-
-    console.log(successMessage);
+  if (command in commands) {
+    commands[command](options);
+  } else {
+    throw new Error(styleText("red", `There is no such command: ${command}`));
   }
-}
-
-/**
- * Add Storybook commands to package.json.
- */
-async function addStorybookCommands() {
-  try {
-    const storybookCommands = {
-      "sb:dev": "storybook dev -p 6006 --no-open",
-      "sb:dev:docs": "storybook dev -p 6006 --docs --no-open",
-      "sb:build": "storybook build --docs",
-      "sb:preview": "vite preview --host --outDir=storybook-static",
-    };
-
-    const packageJsonPath = path.resolve(cwd(), "package.json");
-    const data = await promises.readFile(packageJsonPath, "utf8");
-    const packageJson = JSON.parse(data);
-
-    packageJson.scripts = { ...packageJson.scripts, ...storybookCommands };
-
-    await promises.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2) + "\n", "utf8");
-  } catch (error) {
-    console.error("Error:", error);
-  }
+} catch (error) {
+  console.error(styleText("red", error.message));
+  process.exit(error.code || FAILURE_CODE);
 }
