43d90f99aa405536a410d71c01e1885cc99b305c
Johnny Grid <ilevel.ws@gmail.com>
2025-08-26 09:24:43 +0800

    Added TypeScript configuration files with tailored settings for Node and application builds, declared custom module types, and updated dependencies for compatibility improvements.

---
+++ env.d.ts
@@ -0,0 +1,9 @@
+/// <reference lib="dom" />
+/// <reference lib="dom.iterable" />
+/// <reference types="vite/client" />
+/// <reference types="vueless/modules" />
+
+declare module "@vueless/storybook" {
+  export function storyDarkModeDecorator();
+  export function vue3SourceDecorator();
+}
