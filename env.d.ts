103342f0a7ec444604f88e06138c3a4a2a33d1af
Johnny Grid <ilevel.ws@gmail.com>
2025-08-26 11:24:04 +0800

    Removed unused `@vueless/storybook` module declarations from `env.d.ts` to clean up TypeScript definitions.

--- env.d.ts
+++ env.d.ts
@@ -2,8 +2,3 @@
 /// <reference lib="dom.iterable" />
 /// <reference types="vite/client" />
 /// <reference types="vueless/modules" />
-
-declare module "@vueless/storybook" {
-  export function storyDarkModeDecorator();
-  export function vue3SourceDecorator();
-}
