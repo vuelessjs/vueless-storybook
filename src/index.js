c9db51a3617eeeda4a2ffcc7dbe344c45193b762
Johnny Grid <ilevel.ws@gmail.com>
2025-11-28 14:02:58 +0800

    fix: relocate theme exports to a dedicated "themes" module to avoid storybook warnings

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 export { buildWebTypes } from "./webTypes/index.js";
-export { getThemeDark } from "./theme/getThemeDark.js";
-export { getThemeLight } from "./theme/getThemeLight.js";
-export { getThemeLightPreview } from "./theme/getThemeLightPreview.js";
 
 import { getVuelessConfig } from "vueless/utils/node/vuelessConfig.js";
 import {
---
+++ src/themes.d.ts
@@ -0,0 +1,4 @@
+export { getThemeDark } from "./theme/getThemeDark.js";
+export { getThemeLight } from "./theme/getThemeLight.js";
+export { getThemeLightPreview } from "./theme/getThemeLightPreview.js";
+
